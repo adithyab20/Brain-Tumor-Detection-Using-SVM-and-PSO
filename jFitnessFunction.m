22222f245f144cb5b322248e5686f61406c55cd3
adithyab20 <adithyareddyboravelly@gmail.com>
2025-02-28 02:11:27 -0600

    Add files via upload

---
+++ jFitnessFunction.m
@@ -0,0 +1,16 @@
+function fitness=jFitnessFunction(feat,label,X)
+% Parameter setting for k-value of KNN
+k=5; 
+% Parameter setting for number of cross-validation
+kfold=2;
+% Error rate
+fitness=jwrapperKNN(feat(:,X==1),label,k,kfold);
+end
+
+% Perform KNN with k-folds cross-validation
+function ER=jwrapperKNN(feat,label,k,kfold)
+Model=fitcknn(feat,label,'NumNeighbors',k,'Distance','euclidean'); 
+C=crossval(Model,'KFold',kfold);
+% Error rate 
+ER=kfoldLoss(C);
+end
